c3eff140324bfbf61f378468ebd5199280c3ef0d
aaartyomsss <59570358+aaartyomsss@users.noreply.github.com>
2022-07-28 21:42:09 +0300

    Add redis as caching backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -54,6 +54,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CACHES = {
+    'default': {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
+    }
+}
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
